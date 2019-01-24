b9a2850c9baaacbb20c25609db2ec612ae83c0a0
ConorHamilton19 <conhamil19@gmail.com>
2019-01-24 14:15:08 -0600

    adding user signup system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+
 end
