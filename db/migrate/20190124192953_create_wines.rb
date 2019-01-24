6143cdbf73dcabdf5492c3cc3875fcd9b3a055be
ConorHamilton19 <conhamil19@gmail.com>
2019-01-24 13:32:21 -0600

    resource added for wine using generator

---
+++ db/migrate/20190124192953_create_wines.rb
@@ -0,0 +1,11 @@
+class CreateWines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :wines do |t|
+      t.string :name
+      t.integer :year
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
