260be4841816da16a1b026fc5cd30dbc74ef043c
ConorHamilton19 <conhamil19@gmail.com>
2019-01-24 14:41:42 -0600

    adding login functionality with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :users
 
   root 'welcome#welcome'
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
