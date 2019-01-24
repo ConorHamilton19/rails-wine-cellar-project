e93cbfaf0fc67c5a4544b14726f4867f8b055963
ConorHamilton19 <conhamil19@gmail.com>
2019-01-24 16:41:47 -0600

    trouble with saving wine to database without type- probably because of nested route

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,2 +1,29 @@
 class TypesController < ApplicationController
+
+  def new
+    @type = Type.new
+  end
+
+  def create
+    @type = Type.create(type_params)
+    if @type.save
+      redirect_to type_path(@type)
+    else
+      render new_type_path
+    end
+  end
+
+  def index
+    @types = Type.all
+  end
+
+  def show
+    @type = Type.find(params[:id])
+  end 
+
+  private
+
+  def type_params
+    params.require(:type).permit(:name, :region)
+  end
 end
--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,2 +1,40 @@
 class WinesController < ApplicationController
+
+  def new
+      @wine = Wine.new
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    @wine.save!
+    if @wine.save
+      redirect_to wine_path(@wine)
+    else
+      redirect_to root_path
+    end
+
+  end
+
+  def show
+
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:name, :year, :price)
+  end
+
 end
