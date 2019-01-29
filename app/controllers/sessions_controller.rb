c13c9649cefa1cfcfbb9848db6e5d3d104cb1838
ConorHamilton19 <conhamil19@gmail.com>
2019-01-28 20:32:54 -0600

    trying to make github oauth work- cant get past request phase initiated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 
 
   def create
+    raise stop.inspect
     @user = User.find_by(name: params[:user][:name])
      if @user &&  @user.authenticate(params[:user][:password])
        session[:user_id] = @user.id
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy'
+
+  get '/auth/github/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
