8a9436f5fcf696c48f772d3ff22e20227984e354
ConorHamilton19 <conhamil19@gmail.com>
2019-01-28 21:24:22 -0600

    omniauth github working to log me in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,31 @@ class SessionsController < ApplicationController
 
 
   def create
-    raise stop.inspect
-    @user = User.find_by(name: params[:user][:name])
-     if @user &&  @user.authenticate(params[:user][:password])
-       session[:user_id] = @user.id
-       redirect_to user_path(@user)
-     else
-       redirect_to root_path
-     end
-   end
+    if auth_hash = request.env["omniauth.auth"]["info"]["name"]
+      name = "#{auth_hash.partition(" ").first}"
+      if  @user = User.find_by(name: name)
+          session[:user_id] = @user.id
+          redirect_to root_path
+      else
+        @user = User.new(name: name, password: SecureRandom.hex)
+        if user.save
+          session[:user_id] = @user.id
+          redirect to root_path
+        else
+          raise @user.errors.full_messages.inspect
+        end
+      end
+
+    else
+      @user = User.find_by(name: params[:user][:name])
+      if @user &&  @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to root_path
+      end
+    end
+  end
 
   def destroy
     session.delete :user_id
